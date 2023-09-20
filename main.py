261f598dd2d3e10e06499769abde915fa231decb
Jacob Horton <jacob.horton@ku.edu>
2023-09-20 09:01:44 -0500

    refactored code

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pygame
 
 from settings import Settings
 from character import Character
+from map import Map
 
 class Game:
     # Overall class to manage game assets and behavior.
-
     def __init__(self):
         # Initialize the game, and create resources.
         pygame.init()
@@ -21,6 +21,8 @@ class Game:
 
         self.character = Character(self.screen)
 
+        self.map = Map(self.settings, self.screen)
+
     def run_game(self):
         # Start the main loop for the game.
         while True:
@@ -33,25 +35,39 @@ class Game:
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
+                    pygame.quit()
                     sys.exit()
 
-        # Check for keys that are continously pressed
+        # Check for keys that are continuously pressed
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.character.y -= 2
-        if keys[pygame.K_s]:
-            self.character.y += 2
-        if keys[pygame.K_a]:
-            self.character.x -= 2
-        if keys[pygame.K_d]:
-            self.character.x += 2
+        if keys[self.settings.key_up]:
+            self.character.y -= self.settings.move_speed
+            if keys[self.settings.key_sprint]:
+                self.character.y -= self.settings.sprint_speed
+        if keys[self.settings.key_down]:
+            self.character.y += self.settings.move_speed
+            if keys[self.settings.key_sprint]:
+                self.character.y += self.settings.sprint_speed
+        if keys[self.settings.key_left]:
+            self.character.x -= self.settings.move_speed
+            if keys[self.settings.key_sprint]:
+                self.character.x -= self.settings.sprint_speed
+        if keys[self.settings.key_right]:
+            self.character.x += self.settings.move_speed
+            if keys[self.settings.key_sprint]:
+                self.character.x += self.settings.sprint_speed
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
+
+        self.map.draw()
+
         self.character.draw()
+
         pygame.display.flip()
 
 if __name__ == '__main__':
