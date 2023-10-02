f91baea065e00c1406077e42264728ab994b5621
Jacob Horton <jacob.horton@ku.edu>
2023-10-02 12:32:16 -0500

    cleanup main menu assets

--- main_menu.py
+++ main_menu.py
@@ -7,76 +7,76 @@ class MainMenu:
         self.display_surface = pygame.display.get_surface()#get the surface to draw
         self.origin = (0,0)#define the origin
 
-        self.title = pygame.image.load("./assets/goodtitle.png")#load the png of the title of the game
+        self.title = pygame.image.load("./assets/main_menu/goodtitle.png")#load the png of the title of the game
         self.title_width = self.title.get_width()#get width of title png
         self.title_height = self.title.get_height()#get height of title png
         self.title_pos = (SCREEN_WIDTH/2 - (self.title_width/2), 25)#define the position of the title png
 
-        self.exit_button = pygame.image.load("./assets/exitbutton.png")#load exit button png
-        self.highlighted_exit = pygame.image.load("./assets/exitbutton_highlighted.png")#load highlighted exit button
+        self.exit_button = pygame.image.load("./assets/main_menu/exitbutton.png")#load exit button png
+        self.highlighted_exit = pygame.image.load("./assets/main_menu/exitbutton_highlighted.png")#load highlighted exit button
         self.exit_button_width = self.exit_button.get_width()#get width of the exit png
         self.exit_button_height = self.exit_button.get_height()#get height of the exit png
         self.exit_button_pos = (SCREEN_WIDTH/2 - (self.exit_button_width/2), SCREEN_HEIGHT - (25 + self.exit_button_height))#define position of exit button (25 pix up and in middle)
 
-        self.play_button = pygame.image.load("./assets/play_button.png")#load the play button png
-        self.highlighted_play = pygame.image.load("./assets/play_button_highlighted.png")#load highlihgted play button
+        self.play_button = pygame.image.load("./assets/main_menu/play_button.png")#load the play button png
+        self.highlighted_play = pygame.image.load("./assets/main_menu/play_button_highlighted.png")#load highlihgted play button
         self.play_button_width = self.play_button.get_width()#get the width of the play button
         self.play_button_height = self.play_button.get_height()#get the height of the play button
         self.play_button_pos = (SCREEN_WIDTH/2 - (self.play_button_width/2), self.exit_button_pos[1] - (10 + self.play_button_height))#define position of play button (10 above exit)
 
-        self.settings_button = pygame.image.load("./assets/settingsbutton.png")#load the settings button png
-        self.highlighted_settings = pygame.image.load("./assets/settingsbutton_highlighted.png")#load highlihgted settings button
+        self.settings_button = pygame.image.load("./assets/main_menu/settingsbutton.png")#load the settings button png
+        self.highlighted_settings = pygame.image.load("./assets/main_menu/settingsbutton_highlighted.png")#load highlihgted settings button
         self.settings_button_width = self.settings_button.get_width()#get width of settings button
         self.settings_button_height = self.settings_button.get_height()#get height of settings button
         self.settings_button_pos = ((self.play_button_pos[0] - (10 + self.settings_button_width)), (self.play_button_pos[1] + self.play_button_height + 5 - (self.settings_button_height/2)))#define position of settings button
 
-        self.credits_button = pygame.image.load("./assets/creditsbutton.png")#load the credits button png
-        self.highlighted_credits = pygame.image.load("./assets/creditsbutton_highlighted.png")#load highlighted credits button
+        self.credits_button = pygame.image.load("./assets/main_menu/creditsbutton.png")#load the credits button png
+        self.highlighted_credits = pygame.image.load("./assets/main_menu/creditsbutton_highlighted.png")#load highlighted credits button
         self.credits_button_width = self.credits_button.get_width()#get width of credits button
         self.credits_button_height = self.credits_button.get_height()#get height of credits button
         self.credits_button_pos = ((self.play_button_pos[0] + self.play_button_width + 10), (self.settings_button_pos[1]))#define position of credits button
 
-        self.code_bi = pygame.image.load("./assets/bi_for_code.png")#load the png of word code in binary
+        self.code_bi = pygame.image.load("./assets/main_menu/bi_for_code.png")#load the png of word code in binary
         self.code_bi_width = self.code_bi.get_width()#get width of code binary
         self.code_bi_x = 0#x position of code binary png
         self.code_bi_y = (SCREEN_HEIGHT/6) - 40#y position of code binary png
-        self.code_bi_2 = pygame.image.load("./assets/bi_for_code.png")#load the 2nd png of word code in binary
+        self.code_bi_2 = pygame.image.load("./assets/main_menu/bi_for_code.png")#load the 2nd png of word code in binary
         self.code_bi_2_width = self.code_bi_width#sets width to the same as the first
         self.code_bi_2_x = 0 + self.code_bi_width#sets the image location to the end of the first code bi
         self.code_bi_2_y = self.code_bi_y#sets to the same y position as the first one
 
-        self.of_bi = pygame.image.load("./assets/of_bi.png")#load the png of word of in binary
+        self.of_bi = pygame.image.load("./assets/main_menu/of_bi.png")#load the png of word of in binary
         self.of_bi_width = self.of_bi.get_width()#gets width of of_bi binary
         self.of_bi_x = 0#x position of binary png
         self.of_bi_y = 2*(SCREEN_HEIGHT/6) - 40#y position of of_binary png
-        self.of_bi_2 = pygame.image.load("./assets/of_bi.png")#load the png of word of in binary
+        self.of_bi_2 = pygame.image.load("./assets/main_menu/of_bi.png")#load the png of word of in binary
         self.of_bi_2_width = self.of_bi_width#gets width of of_bi binary
         self.of_bi_2_x = self.of_bi_x - self.of_bi_2_width#x position of binary png
         self.of_bi_2_y = self.of_bi_y#y position of of_binary png
 
-        self.the_bi = pygame.image.load("./assets/the_bi.png")#load the png of word the in binary
+        self.the_bi = pygame.image.load("./assets/main_menu/the_bi.png")#load the png of word the in binary
         self.the_bi_width = self.the_bi.get_width()#gets width of the_bi binary
         self.the_bi_x = 0#x position of the binary png
         self.the_bi_y = 3*(SCREEN_HEIGHT/6) - 40#y position of the_binary png
-        self.the_bi_2 = pygame.image.load("./assets/the_bi.png")#load the png of word the in binary
+        self.the_bi_2 = pygame.image.load("./assets/main_menu/the_bi.png")#load the png of word the in binary
         self.the_bi_2_width = self.the_bi_width#gets width of the_bi binary
         self.the_bi_2_x = 0 + self.the_bi_width#x position of the binary png
         self.the_bi_2_y = self.the_bi_y#y position of the_binary png
 
-        self.undead_bi = pygame.image.load("./assets/Undead_bi.png")#load the png of word undead in binary
+        self.undead_bi = pygame.image.load("./assets/main_menu/Undead_bi.png")#load the png of word undead in binary
         self.undead_bi_width = self.undead_bi.get_width()#gets width of undead_bi binary
         self.undead_bi_x = 0#x position of undead binary png
         self.undead_bi_y = 4*(SCREEN_HEIGHT/6) - 40#y position of undead_binary png
-        self.undead_bi_2 = pygame.image.load("./assets/undead_bi.png")#load the png of word undead in binary
+        self.undead_bi_2 = pygame.image.load("./assets/main_menu/undead_bi.png")#load the png of word undead in binary
         self.undead_bi_2_width = self.undead_bi_width#gets width of undead_bi binary
         self.undead_bi_2_x = 0 + self.undead_bi_width#x position of undead binary png
         self.undead_bi_2_y = self.undead_bi_y#y position of undead_binary png
 
-        self.initials_bi = pygame.image.load("./assets/GSNJE_bi.png")#load the png of word undead in binary
+        self.initials_bi = pygame.image.load("./assets/main_menu/GSNJE_bi.png")#load the png of word undead in binary
         self.initials_bi_width = self.initials_bi.get_width()#gets width of undead_bi binary
         self.initials_bi_x = 0#x position of undead binary png
         self.initials_bi_y = 5*(SCREEN_HEIGHT/6) - 40#y position of undead_binary png
-        self.initials_bi_2 = pygame.image.load("./assets/GSNJE_bi.png")#load the png of word undead in binary
+        self.initials_bi_2 = pygame.image.load("./assets/main_menu/GSNJE_bi.png")#load the png of word undead in binary
         self.initials_bi_2_width = self.initials_bi_width#gets width of undead_bi binary
         self.initials_bi_2_x = self.initials_bi_x - self.initials_bi_2_width#x position of undead binary png
         self.initials_bi_2_y = self.initials_bi_y#y position of undead_binary png
